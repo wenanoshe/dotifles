94bed2b667474c32ca8ed975285ffe49c6274066
wenanoshe <braiderandreyp@gmail.com>
2023-07-04 14:56:20 -0500

    New backup 04-07-2023 14:56:20

--- neovim/custom/chadrc.lua
+++ neovim/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "gruvbox",
-  theme_toggle = { "gruvbox", "gruvbox_light" },
+  theme = "onedark",
+  theme_toggle = { "onedark", "onedark_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
