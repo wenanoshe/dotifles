9537bdf6ab69e86b021692716354cfb68c1c4022
Braider Perez <braiderandreyp@gmail.com>
2023-07-04 16:52:18 -0500

    New backup 04-07-2023 16:52:18

--- neovim/custom/chadrc.lua
+++ neovim/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "chadtain",
-  theme_toggle = { "chadtain", "chadtain_light" },
+  theme = "bearded-arc",
+  theme_toggle = { "bearded-arc", "bearded-arc_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
